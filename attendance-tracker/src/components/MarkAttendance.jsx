99abdbe4ea54eab8998e1a3883219d85f430a122
Pramothsgp <jarvis.tech.006@gmail.com>
2025-09-17 22:59:15 +0530

    chore: decode base64

--- attendance-tracker/src/components/MarkAttendance.jsx
+++ attendance-tracker/src/components/MarkAttendance.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react'
 import { use } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useRoutes } from 'react-router-dom'
 import { useState } from 'react'
-import { base64 } from 'base-64'
+import base64 from 'base-64';
 
 const MarkAttendance = () => {
-    const {token} = useLocation().searchParams;
+    const {token} = Object.fromEntries(new URLSearchParams(window.location.search));
     const [data , setData] = useState(null);
     // const base64 = require('base-64');
     useEffect(() => {
         console.log(token)
         const decode  = base64.decode(token)
         console.log(decode)
+        setData(decode)
     },[]);
   return (
     <div>{data ?? <p>No attendance data available</p>}</div>
